15eb9e4c21ed39026accd855d9a3fc244cc3bee4
entroopie <marcel.boxberger@gmail.com>
2025-10-03 11:55:37 +0200

    Added fundaments :)

---
+++ backend/app/models.py
@@ -0,0 +1,46 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    location_id: int = Field(foreign_key="location.id")
+    description: str
+    datetime: datetime
+    priority: int
+    status: str
+
+class ResourceNeeded(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    resource_type: str
+    description: str
+    quantity: int
+    is_fulfilled: bool
+    event_id: int = Field(foreign_key="event.id")
+
+class ResourceAvailable(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    resource_type: str
+    quantity: int
+    description: str
+    status: str
+    volunteer_id: int = Field(foreign_key="volunteer.id")
+    is_allocated: bool
+
+class Volunteer(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    email: str
+    phonenumber: str
+    availability: str
+    event_id: int = Field(default=None, foreign_key="event.id")
+    location_id: int = Field(default=None, foreign_key="location.id")
+
+class Location(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    region: str
+    address: str
+    postcode: str
+    latitude: float
+    longitude: float
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicMap from './maps/BasicMap';
+
+function App() {
+  return (
+    <div>
+      <h1>My Leaflet.js and React Map</h1>
+      <BasicMap />
+    </div>
+  );
+}
+export default App;
