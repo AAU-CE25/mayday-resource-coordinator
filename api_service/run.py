e1ae282c25bf70f64101fc8723e01520678bc799
KajetanPawlowski <kpawlowski1007@gmail.com>
2025-10-05 19:29:56 +0200

    added postgeSQL DB

--- api_service/run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-
-def run():
-    uvicorn.run(
-        "app.main:app",  # module:app
-        host="0.0.0.0",
-        port=8000,
-        reload=True  # auto-reload on code changes (development mode)
-    )
