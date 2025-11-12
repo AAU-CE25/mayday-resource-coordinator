10b01bc1383b5bef97cfa5678162b8218564fde0
entroopie <marcel.boxberger@gmail.com>
2025-11-12 13:21:08 +0100

    notification hook change

--- frontend/hooks/use-notifications.ts
+++ frontend/hooks/use-notifications.ts
@@ -1,27 +1,14 @@
-"use client"
-
-import useSWR from "swr"
-
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
-
-export function useNotifications() {
-  return useSWR("/api/notifications", fetcher, {
-    refreshInterval: 3000, // Check for new notifications every 3 seconds
-  })
-}
-
-
 // NEW HOOK IMPLEMENTATION
 
-// 'use client'
+'use client'
 
-// import { useQuery } from '@tanstack/react-query'
-// import { api } from '@/lib/api-client'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/api-client'
 
-// export function useEvents() {
-//   return useQuery({
-//     queryKey: ['events'],
-//     queryFn: () => api.get('/events/'),
-//     refetchInterval: 5000
-//   })
-// }
+export function useNotifications() {
+  return useQuery({
+    queryKey: ['events'],
+    queryFn: () => api.get('/notifications/'),
+    refetchInterval: 5000
+  })
+}
